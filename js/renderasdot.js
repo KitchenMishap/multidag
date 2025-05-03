c2cde62dfd1e4b26956a819165e1b9234cf88e7c
KitchenMishap <hello@kitchenmishap.art>
2025-05-03 05:18:57 +0100

    Delete dead code

--- js/renderasdot.js
+++ js/renderasdot.js
@@ -1,26 +1,5 @@
 // Functions that render things in the "dot" graph lanuage
 
-function renderSingleHtmlStringAsDot(str) {
-  var result = "";
-
-  // Start of the dot string
-  result += 'digraph {\n';
-
-  // The node
-  result += 'thesinglevertex';
-  result += ' [labelType="html" label="';
-  result += str;
-  result += '"];\n';
-
-  // The end of the dot string
-  result += '}\n';
-
-  inputGraph.value = result;
-  tryDraw();
-
-  return
-}
-
 function renderAllAsDot() {
   multiMetaVertices = {};
   var result = "";
